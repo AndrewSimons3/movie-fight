032927489de35960d6df4c5d3a2bc1d4d4875e7b
AndrewSimons3 <simdrew07@gmail.com>
2021-08-04 12:51:48 -0500

    returning movie images from fetch

--- index.js
+++ index.js
@@ -7,26 +7,31 @@ const fetchData = async (searchTerm) => {
     }
   });
 
-  console.log(response.data)
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
+
+
+
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+    console.log(movies);
+    for (let movie of movies) {
+      const div = document.createElement('div');
+      div.innerHTML = `
+        <img src="${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+      `;
+
+      document.querySelector('#target').appendChild(div);
     }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay)
-  };
-};
-
-
-const onInput = event => {
-    fetchData(event.target.value);
+    
   };
 
   
