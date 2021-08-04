340945892f4349ce7aefca1d2262a1d5644c7f72
AndrewSimons3 <simdrew07@gmail.com>
2021-08-03 19:46:55 -0500

    fetched avengers movies

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '8993b35f',
+      s: 'avengers'
+    }
+  });
+
+  console.log(response.data)
+};
+
+fetchData();
