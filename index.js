3845d44285c4433d36b2aac53809034ae1959c6d
AndrewSimons3 <simdrew07@gmail.com>
2021-08-05 19:49:16 -0500

    follow up request wtih api to gather more information

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ const onInput = async event => {
       option.addEventListener('click', () => {
         dropdown.classList.remove('is-active');
         input.value = movie.Title;
+        onMovieSelect(movie)
       });
 
       resultsWrapper.appendChild(option);
@@ -63,3 +64,14 @@ document.addEventListener('click', event => {
     dropdown.classList.remove('is-active');
   }
 });
+
+const onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '8993b35f',
+      i: movie.imdbID,
+    }
+  });
+
+  console.log(response.data)
+};
